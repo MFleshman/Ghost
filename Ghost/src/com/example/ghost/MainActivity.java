523eeb61093cf88a82b6ebba3df362308aad82f3
Collin Berman <cmb5nh@virginia.edu>
2013-10-29 20:37:04 -0400

    added test comment

--- Ghost/src/com/example/ghost/MainActivity.java
+++ Ghost/src/com/example/ghost/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	//test
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
